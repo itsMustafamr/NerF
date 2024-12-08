391393eec4d321017e963377fb6e7db31bc24a4b
mohd7 <mohd7@iastate.edu>
2024-12-08 15:32:21 -0600

    Model study with global model declaration done and works on rendering voxel

--- rendering.py
+++ rendering.py
@@ -1,57 +1,57 @@
-import torch
-
-def compute_accumulated_transmittance(betas): #takes the cummulative product of transmittance along the ray
-    accumulated_transmittance = torch.cumprod(betas, 1)     
-    return torch.cat((torch.ones(accumulated_transmittance.shape[0], 1, device=accumulated_transmittance.device),
-                      accumulated_transmittance[:, :-1]), dim=1)
-
-def rendering(model, rays_o, rays_d, tn, tf, nb_bins=100, device='cpu'):
-    
-    t = torch.linspace(tn, tf, nb_bins).to(device) # [nb_bins]
-    delta = torch.cat((t[1:] - t[:-1], torch.tensor([1e10])))
-    
-    x = rays_o.unsqueeze(1) + t.unsqueeze(0).unsqueeze(-1) * rays_d.unsqueeze(1) # [nb_rays, nb_bins, 3]
-    
-    colors, density = model.intersect(x.reshape(-1, 3))
-    
-    colors = colors.reshape((x.shape[0], nb_bins, 3)) # [nb_rays, nb_bins, 3]
-    
-    alpha = 1 - torch.exp(- density * delta.unsqueeze(0)) # [nb_rays, nb_bins, 1]
-    T = compute_accumulated_transmittance(1 - alpha) # [nb_rays, nb_bins, 1]
-
-    c = (T.unsqueeze(-1) * alpha.unsqueeze(-1) * colors).sum(1) # [nb_rays, 3]
-    
-    return c
-
-#code given by yt
 # import torch
 
-# def compute_accumulated_transmittance(betas):
+# def compute_accumulated_transmittance(betas): #takes the cummulative product of transmittance along the ray
 #     accumulated_transmittance = torch.cumprod(betas, 1)     
 #     return torch.cat((torch.ones(accumulated_transmittance.shape[0], 1, device=accumulated_transmittance.device),
 #                       accumulated_transmittance[:, :-1]), dim=1)
 
-# def rendering(model, rays_o, rays_d, tn, tf, nb_bins=100, device='cpu', white_bckgr=True):
+# def rendering(model, rays_o, rays_d, tn, tf, nb_bins=100, device='cpu'):
     
 #     t = torch.linspace(tn, tf, nb_bins).to(device) # [nb_bins]
-#     delta = torch.cat((t[1:] - t[:-1], torch.tensor([1e10], device=device)))
+#     delta = torch.cat((t[1:] - t[:-1], torch.tensor([1e10])))
     
-#     x = rays_o.unsqueeze(1) + t.unsqueeze(0).unsqueeze(-1) * rays_d.unsqueeze(1) # [nb_rays, nb_bins, 3]    
+#     x = rays_o.unsqueeze(1) + t.unsqueeze(0).unsqueeze(-1) * rays_d.unsqueeze(1) # [nb_rays, nb_bins, 3]
     
-#     colors, density = model.intersect(x.reshape(-1, 3), rays_d.expand(x.shape[1], x.shape[0], 3).transpose(0, 1).reshape(-1, 3))
+#     colors, density = model.intersect(x.reshape(-1, 3))
     
 #     colors = colors.reshape((x.shape[0], nb_bins, 3)) # [nb_rays, nb_bins, 3]
-#     density = density.reshape((x.shape[0], nb_bins))
     
 #     alpha = 1 - torch.exp(- density * delta.unsqueeze(0)) # [nb_rays, nb_bins, 1]
-        
-#     weights = compute_accumulated_transmittance(1 - alpha) * alpha # [nb_rays, nb_bins]
-    
-#     if white_bckgr:
-#         c = (weights.unsqueeze(-1) * colors).sum(1) # [nb_rays, 3]
-#         weight_sum = weights.sum(-1) # [nb_rays]
-#         return c + 1 - weight_sum.unsqueeze(-1)
-#     else:
-#         c = (weights.unsqueeze(-1) * colors).sum(1) # [nb_rays, 3]
+#     T = compute_accumulated_transmittance(1 - alpha) # [nb_rays, nb_bins, 1]
+
+#     c = (T.unsqueeze(-1) * alpha.unsqueeze(-1) * colors).sum(1) # [nb_rays, 3]
     
 #     return c
+
+# code given by yt
+import torch
+
+def compute_accumulated_transmittance(betas):
+    accumulated_transmittance = torch.cumprod(betas, 1)     
+    return torch.cat((torch.ones(accumulated_transmittance.shape[0], 1, device=accumulated_transmittance.device),
+                      accumulated_transmittance[:, :-1]), dim=1)
+
+def rendering(model, rays_o, rays_d, tn, tf, nb_bins=100, device='cpu', white_bckgr=True):
+    
+    t = torch.linspace(tn, tf, nb_bins).to(device) # [nb_bins]
+    delta = torch.cat((t[1:] - t[:-1], torch.tensor([1e10], device=device)))
+    
+    x = rays_o.unsqueeze(1) + t.unsqueeze(0).unsqueeze(-1) * rays_d.unsqueeze(1) # [nb_rays, nb_bins, 3]    
+    
+    colors, density = model.intersect(x.reshape(-1, 3), rays_d.expand(x.shape[1], x.shape[0], 3).transpose(0, 1).reshape(-1, 3))
+    
+    colors = colors.reshape((x.shape[0], nb_bins, 3)) # [nb_rays, nb_bins, 3]
+    density = density.reshape((x.shape[0], nb_bins))
+    
+    alpha = 1 - torch.exp(- density * delta.unsqueeze(0)) # [nb_rays, nb_bins, 1]
+        
+    weights = compute_accumulated_transmittance(1 - alpha) * alpha # [nb_rays, nb_bins]
+    
+    if white_bckgr:
+        c = (weights.unsqueeze(-1) * colors).sum(1) # [nb_rays, 3]
+        weight_sum = weights.sum(-1) # [nb_rays]
+        return c + 1 - weight_sum.unsqueeze(-1)
+    else:
+        c = (weights.unsqueeze(-1) * colors).sum(1) # [nb_rays, 3]
+    
+    return c
