052a5a0c2eefba46192fade6cc943a1696062700
mohd7 <mohd7@iastate.edu>
2024-12-21 18:43:46 -0600

    added training.py file to run nerf training

---
+++ training.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import imageio
+import matplotlib.pyplot as plt
+import os
+from torch.utils.data import DataLoader
+from rendering import rendering
+from model import Voxels
+from tqdm import tqdm
+from ml_helpers import training 
+from dataset import get_rays
+
+class Nerf(nn.Module):
+    
+    def __init__(self, Lpos=10, Ldir=4, hidden_dim=256):
+        super(Nerf, self).__init__()
+        
+        self.block1 = nn.Sequential(nn.Linear(Lpos * 6 + 3, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU())
+        
+        self.block2 = nn.Sequential(nn.Linear(hidden_dim + Lpos * 6 + 3, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                    nn.Linear(hidden_dim, hidden_dim + 1),) #256 + 1(sigma)
+        
+        self.rgb_head = nn.Sequential(nn.Linear(hidden_dim + Ldir * 6 + 3, hidden_dim // 2), nn.ReLU(),
+                                      nn.Linear(hidden_dim // 2, 3), nn.Sigmoid())
+        
+        self.Lpos = Lpos
+        self.Ldir = Ldir
+        
+    def positional_encoding(self, x, L):
+        out = [x]
+        for j in range(L):
+            out.append(torch.sin(2 ** j * x))
+            out.append(torch.cos(2 ** j * x))
+        return torch.cat(out, dim=1)
+            
+                                    
+        
+    def forward(self, xyz, d):
+        
+        x_emb = self.positional_encoding(xyz, self.Lpos) # [batch_size, Lpos * 6 + 3]
+        d_emb = self.positional_encoding(d, self.Ldir) # [batch_size, Ldir * 6 + 3]
+        
+        h = self.block1(x_emb) # [batch_size, hidden_dim]
+        h = self.block2(torch.cat((h, x_emb), dim=1)) # [batch_size, hidden_dim + 1]
+        sigma = h[:, -1]
+        h = h[:, :-1] # [batch_size, hidden_dim]
+        c = self.rgb_head(torch.cat((h, d_emb), dim=1))
+        
+        return c, torch.relu(sigma)
+        
+    
+    def intersect(self, x, d):
+        return self.forward(x, d)
+
+batch_size = 1024
+
+# Train dataloader
+o, d, target_px_values = get_rays('fox', mode='train')
+dataloader = DataLoader(torch.cat((
+    torch.from_numpy(o).reshape(-1, 3).type(torch.float),
+    torch.from_numpy(d).reshape(-1, 3).type(torch.float),
+    torch.from_numpy(target_px_values).reshape(-1, 3).type(torch.float)
+), dim=1), batch_size=batch_size, shuffle=True)
+
+# Warmup dataloader
+dataloader_warmup = DataLoader(torch.cat((
+    torch.from_numpy(o).reshape(90, 400, 400, 3)[:, 100:300, 100:300, :].reshape(-1, 3),
+    torch.from_numpy(d).reshape(90, 400, 400, 3)[:, 100:300, 100:300, :].reshape(-1, 3),
+    torch.from_numpy(target_px_values).reshape(90, 400, 400, 3)[:, 100:300, 100:300, :].reshape(-1, 3)
+), dim=1), batch_size=batch_size, shuffle=True)
+
+# Test dataloader
+test_o, test_d, test_target_px_values = get_rays('fox', mode='test')
+
+device = 'cuda'
+nb_epochs = 1
+lr = 1e-3
+tn = 8
+tf = 12
+nb_bins = 100
+gamma = 0.5
+
+model = Nerf()
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[5, 10], gamma=gamma) # torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
+
+
+
+training_loss = training(model, optimizer, scheduler, tn, tf, nb_bins, nb_epochs, dataloader, device=device)
